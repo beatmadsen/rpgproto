48d89a0a5b3f785fef34bc3f444bf85b7c9ff1fb
Erik Madsen <beatmadsen@gmail.com>
2015-10-11 20:43:57 +0200

    EM: Last commit before taking in Akka

--- src/main/scala/com/madsen/gameproto/model/GameLoop.scala
+++ src/main/scala/com/madsen/gameproto/model/GameLoop.scala
@@ -1,6 +1,9 @@
 package com.madsen.gameproto.model
 
-import scala.concurrent.Future
+import java.util.concurrent.TimeUnit
+
+import scala.concurrent.duration.Duration
+import scala.concurrent.{Await, Future}
 
 /**
  * Created by erikmadsen2 on 11/10/2015.
@@ -14,28 +17,41 @@ object GameLoop {
   def run(): Unit = {
 
     while (OnOffSwitch.isOn) {
-      Ticker.tick()
-
-      val lag: Double = Ticker.latestLag
 
       // line up user requested changes to game state
-      processInput() // non-blocking, side-effecting -> Future[Unit]
 
-      while (lag >= MillisPerUpdate) {
-        // execute frame; movements, physics, ai
-        update() // non-blocking, side-effecting -> Future[Unit]
-        Ticker.commitWork(MillisPerUpdate)
-      }
+      val steps: List[Future[Unit]] = processInput() ::
+        updateTurn() ::
+        render() ::
+        Nil
 
-      render() // non-blocking, side-effecting -> Future[Unit]
+      val f: Future[List[Unit]] = Future.sequence(steps)
+
+      Await.result(f, Duration(MillisPerUpdate * 10, TimeUnit.MILLISECONDS))
     }
   }
 
 
+  def updateTurn(): Future[Unit] = Future {
+    // NB: Future.apply() is different in scala.concurrent/akka than in Finagle.
+    Ticker.tick()
+
+    var lag: Double = Ticker.latestLag
+    while (lag >= MillisPerUpdate) {
+
+      // execute frame; movements, physics, ai
+      update() // BLOCKING!
+      Ticker.commitWork(MillisPerUpdate)
+
+      lag = Ticker.latestLag
+    }
+  }
+
+
+  def update(): Unit = ???
+
+
   def render(): Future[Unit] = ???
-
-
-  def update(): Future[Unit] = ???
 
 
   def processInput(): Future[Unit] = ???
