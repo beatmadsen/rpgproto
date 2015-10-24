cd63db05813e3c77768ea0dc8162e05a6a361bbd
Erik Madsen <beatmadsen@gmail.com>
2015-10-24 18:19:34 +0200

    EM: Client registry

--- src/main/scala/com/madsen/gameproto/akka/Frontend.scala
+++ src/main/scala/com/madsen/gameproto/akka/Frontend.scala
@@ -2,10 +2,12 @@ package com.madsen.gameproto.akka
 
 import akka.actor._
 import akka.io.Tcp._
-import akka.util.ByteString
+import akka.pattern.ask
+import akka.util.{ByteString, Timeout}
 import com.madsen.gameproto.akka.ClientRegistry._
 import play.api.libs.json._
 
+import scala.concurrent.ExecutionContext
 import scala.util.Try
 
 /**
@@ -14,6 +16,8 @@ import scala.util.Try
 class Frontend(clientRegistry: ActorRef) extends Actor {
 
   val Separator: Seq[Byte] = ByteString("\r\n").toArray.toSeq
+  implicit val timeout: Timeout = Timeout.longToTimeout(8000)
+  implicit val ec: ExecutionContext = context.system.dispatcher
   var count = 0
   var buffer: ByteString = ByteString.empty
 
@@ -51,31 +55,43 @@ class Frontend(clientRegistry: ActorRef) extends Actor {
   }
 
 
-  private def handle(request: RpgRequest): Unit = request match {
+  private def handle(request: RpgRequest): Unit = {
 
-    case RpgRequest("hello", args) ⇒
-      args.get("id") foreach { id ⇒
-        clientRegistry ! RegisterClient(id, sender())
-      }
+    val oldS = sender()
 
-    case RpgRequest("plus", args) ⇒
-      args.get("value") foreach { value ⇒
-        count += value.toInt
-      }
+    request match {
 
-    case RpgRequest("minus", args) ⇒
-      args.get("value") foreach { value ⇒
-        count -= value.toInt
-      }
+      case RpgRequest("hello", args) ⇒
+        args.get("id") foreach { id ⇒
+          clientRegistry ! RegisterClient(id, sender())
+        }
 
-    case RpgRequest("get", _) ⇒
+      case RpgRequest("check", args) ⇒
+        args.get("id") foreach { id ⇒
+          (clientRegistry ? ClientQuery(id)).mapTo[Option[ClientResult]] foreach { mResult ⇒
+            oldS ! Write(ByteString(mResult.toString))
+          }
+        }
 
-      val response: RpgResponse = RpgResponse(count)
-      val jsonOut = Json.toJson(response)
+      case RpgRequest("plus", args) ⇒
+        args.get("value") foreach { value ⇒
+          count += value.toInt
+        }
 
-      sender() ! Write(ByteString(jsonOut.toString()))
+      case RpgRequest("minus", args) ⇒
+        args.get("value") foreach { value ⇒
+          count -= value.toInt
+        }
 
-    case _ ⇒ sender ! Write(ByteString("Unexpected input"))
+      case RpgRequest("get", _) ⇒
+
+        val response: RpgResponse = RpgResponse(count)
+        val jsonOut = Json.toJson(response)
+
+        sender() ! Write(ByteString(jsonOut.toString()))
+
+      case _ ⇒ sender ! Write(ByteString("Unexpected input"))
+    }
   }
 }
 
